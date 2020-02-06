9eb19dd0337559fa4fb2437d2e7a9d0d2881df67
Ilyas Bensalem <ilyasbensalem.adlen@gmail.com>
2020-02-06 19:30:20 +0100

    Remove printf from rsa_file_char.c for a test

--- Code C/rsa_file_char.c
+++ Code C/rsa_file_char.c
@@ -3,13 +3,13 @@
 #include "rsa_header.h"
 #include "Sha-256/sha256_utils.h"
 
- 
+
 
 /*convertion en base 64*/
 
 
-void  RSAfile_crypt(char *inFilename, char *outFilename,rsaKey_t pubKey){ 
-    /*Alogirthme 
+void  RSAfile_crypt(char *inFilename, char *outFilename,rsaKey_t pubKey){
+    /*Alogirthme
 -Ouvrir le fichier sur lequel on va lire le message à encoder en lecture*/
     FILE *inFile= fopen(inFilename, "r");
     if(inFile == NULL){
@@ -17,12 +17,12 @@ void  RSAfile_crypt(char *inFilename, char *outFilename,rsaKey_t pubKey){
     }
      /*
 -Ouvrir le fichier sur lequel on va écire en écriture (le créer s'il n'existe pas)*/
-    
+
     FILE *outFile =  fopen(outFilename,"w");
     if(outFile == NULL){
          erreur("rsa_file_char.c:RSAfile_crypt:ouverture outFilename");
     }
-    
+
    /*
 -mettre toutes les données du fichier en lecture dans une chaine de caractère*/
     //se positionner sur le dernier octet du fichier
@@ -32,30 +32,28 @@ void  RSAfile_crypt(char *inFilename, char *outFilename,rsaKey_t pubKey){
     rewind(inFile);
     uchar buffer[tailleOctetFichier];
     fread(buffer, tailleOctetFichier , 1 ,inFile);
-    
+
     /*-Utiliser fonction d'aurélie void RSAcrypt(unsigned char *msg, uint64 * crypterMsg, rsaKey_t pubKey);*/
     uint64 crypterMsg[tailleOctetFichier];
-    
+
     RSAcrypt(buffer, crypterMsg, pubKey);
     for(int i = 0; i < tailleOctetFichier; i++){
         buffer[i] = crypterMsg[i];
     }
     /*
-    utilisation la fonction donné sur le résultat de la fonction d'aurélie: 
-        char *base64_encode(const unsigned char *data, size_t input_length, size_t ouput_length); 
+    utilisation la fonction donné sur le résultat de la fonction d'aurélie:
+        char *base64_encode(const unsigned char *data, size_t input_length, size_t ouput_length);
         */
     size_t input_length = tailleOctetFichier;
     printf("%d\n", tailleOctetFichier);
     size_t output_length = 0;
-    printf("%s\n", buffer);
+  
     base64_encode(buffer, input_length, &output_length);
     /*
-     * 
+     *
     ecrire la chaine de caractère dans le fichier en écriture
     */
-    
-    printf("%ld\n", output_length);
-    
+
     fwrite(buffer,output_length, 1, outFile);
     fclose(inFile);
     fclose(outFile);
@@ -63,7 +61,7 @@ void  RSAfile_crypt(char *inFilename, char *outFilename,rsaKey_t pubKey){
 
 void RSAfile_decrypt(char* inFilename, char *outFilename, rsaKey_t privKey){
 
-    /*Alogirthme 
+    /*Alogirthme
 -Ouvrir le fichier sur lequel on va lire le message à décoder en lecture*/
     FILE *inFile= fopen(inFilename, "r");
     if(inFile == NULL){
@@ -71,13 +69,13 @@ void RSAfile_decrypt(char* inFilename, char *outFilename, rsaKey_t privKey){
     }
      /*
 -Ouvrir le fichier sur lequel on va écire en écriture (le créer s'il n'existe pas)*/
-    
+
     FILE *outFile =  fopen(outFilename,"w");
     if(outFile == NULL){
          erreur("rsa_file_char.c:RSAfile_decrypt:ouverture outFilename");
     }
 
-    
+
    /*
 -mettre toutes les données du fichier en lecture dans une chaine de caractère*/
     //se positionner sur le dernier octet du fichier
@@ -93,22 +91,21 @@ void RSAfile_decrypt(char* inFilename, char *outFilename, rsaKey_t privKey){
     size_t output_length = 0;
     base64_decode(buffer, input_length, &output_length);
     /*
-     * 
-    
+     *
+
     /*-Utiliser fonction d'aurélie void RSAdecrypt(unsigned char *msg, uint64 * crypterMsg, rsaKey_t pubKey);*/
     uint64 crypterMsg[tailleOctetFichier];
     RSAcrypt(buffer, crypterMsg, privKey);
     for(int i = 0; i < output_length; i++){
         buffer[i] = crypterMsg[i];
     }
-    
+
     /*
     ecrire la chaine de caractère dans le fichier en écriture
     */
-    
+
     fwrite(buffer,tailleOctetFichier, 1, outFile);
     fclose(inFile);
     fclose(outFile);
 }
 }
-
